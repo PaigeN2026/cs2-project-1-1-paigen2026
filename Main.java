2fdf0a05acfaf033029b0efd2d25ed43a0833e3a
PaigeN2026 <Paige.nunez2026@gbwl.org>
2024-09-17 12:58:01 +0000

    edited main

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // PROBLEM 1
+
+      // DECLARE VARIABLES 
+      int distanceKm = 380000;
+      int speedKmSec = 10; // km per sec
+      int speedKmMin = speedKmSec * 60; // km per min
+      int speedKmHr = speedKmMin * 60; // km per hour
+      int timeHr = distanceKm / speedKmHr; // time equation
+
+      // PRINT total hours to get to the moon
+      System.out.println("It would take " + timeHr + " hours to get to the moon." );
+
+      // PROBLEM 2
+
+      // DECLARE VARIABLES 
+      int horiSpeed = 2000;
+      int veriSpeed = 3000;
+
+      // square hori and veri add together and sqrt total
+      int horiSquared 
+      int totalSpeed;
+
+      System.out.println(horiSpeed);
+      System.out.println(result);
+
    }
 }
